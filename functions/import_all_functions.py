bd12bc857af5a80ee7cad49548b8a1a9cfc72db4
Oli Harvey <oliharvey@hotmail.com>
2021-04-11 21:40:37 +0100

    had to move import all function back into notebook

--- functions/import_all_functions.py
+++ functions/import_all_functions.py
@@ -7,6 +7,15 @@ def import_all_functions():
     for func in funcs:
         if func.startswith('__') or func == 'import_all_functions.py':
             continue
-        func = func.replace('.py','')
-        print(f'{func} imported')
-        exec(f'from functions.{func} import *')
+        # func = func.replace('.py','')
+        # print(f'{func} imported')
+        # exec(f'from functions.{func} import *')
+        mod = __import__(f'functions.{func}')
+        to_import = [getattr(mod, x) for x in dir(mod)]
+
+        for i in to_import:
+            try:
+                setattr(sys.modules[__name__], i.__name__, i)
+                print(f'{func} imported')
+            except AttributeError:
+                pass
--- functions/import_to_df.py
+++ functions/import_to_df.py
@@ -1,1 +1,22 @@
-def import 
+import pandas as pd
+
+def import_to_df(
+        list: str,
+        path: str = "competition_data"
+         ) -> dict:
+    """
+        Imports all the files in the list given
+        Returns dictionary with file name minus extension as 
+    """
+
+    df_dict = {}
+    for file in list:
+        df = pd.read_csv("/".join([path, file]))
+        # remove extension
+        name = file.split('.')[0]
+        df_dict[name] = df
+    
+    return df_dict
+[
+
+]
